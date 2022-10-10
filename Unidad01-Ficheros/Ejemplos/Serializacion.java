97480f17beb78f34d863bac9b7d21c94f1805a6c
alejandroroig <a.roigaguilar@edu.gva.es>
2022-10-10 09:29:37 +0200

    Serialización de colecciones

--- Unidad01-Ficheros/Ejemplos/Serializacion.java
+++ Unidad01-Ficheros/Ejemplos/Serializacion.java
@@ -23,8 +23,20 @@ class Empleado implements Serializable
 public class Serializacion {
     public static void main(String[] args) {
         Path nombreFichero = Path.of("Unidad01-Ficheros\\Ejemplos","empleados.dat");
+
+        // En el caso de querer serializar una lista de objetos, lo más intuitivo
+        // es recorrer la colección y serializar uno a uno los objetos que la contienen
+        // Sin embargo, tenemos la opción de serializar colecciones enteras siempre
+        // que los elementos contenidos sean serializables.
+        //
+        // Por ejemplo:
+        // List<Empleado> empleados = new ArrayList<>();
+        // empleados.add(new Empleado(...));
+        // empleados.add(new Empleado(...));
+        // oos.writeObject(empleados);
+        // List<Empleado> empleados2 = (List<Empleado>)(ois.readObject());
         try (FileOutputStream fos = new FileOutputStream(nombreFichero.toFile());
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            ObjectOutputStream oos = new ObjectOutputStream(fos)) {
 
             Empleado emp = new Empleado("Juan Palomo", 25, 1400);
             oos.writeObject(emp);
